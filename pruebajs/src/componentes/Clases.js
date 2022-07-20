cde6c9a3c502cce18d64f743c14b1db3ec0eb440
mianmope <65711147+mianmope@users.noreply.github.com>
2022-07-20 23:58:01 +0200

    Falta funcionalidad de mostrar todas las clases, pero el diseño del componente a iterar esta finalizado a falta de poner radiobuttons o algo similar

--- pruebajs/src/componentes/Clases.js
+++ pruebajs/src/componentes/Clases.js
@@ -3,7 +3,25 @@ import '../estilos/Clases.css';
 function Clases() {
 
     return (
-        <div>HOLAAA CLASES</div>
+        <div className="contenedorClase">
+            <div className="item-top">
+                <h6>Clase nombre</h6>
+                <p>Nombre profesor</p>
+            </div>
+            <div className="item-bot">
+                <div className="item-nivel">
+                    <p>Nivel</p>
+                    <p>...</p>
+
+                </div>
+                <div className="item-fecha">
+                    <p>12 Jun</p>
+                </div>
+                <div className="item-tiempo">
+                    <p>Duración 52'</p>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Clases;
--- pruebajs/src/componentes/Reproductor.js
+++ pruebajs/src/componentes/Reproductor.js
@@ -1,5 +1,6 @@
 import React,{useState, useEffect} from "react";
 import '../estilos/Reproductor.css';
+import {Link} from "react-router-dom";
 import * as utils from "../utils/metodos";
 function Reproductor() {
 
@@ -10,10 +11,29 @@ function Reproductor() {
       },[]);
 
     return (
-        <div>
-            <h2>{datosClase.name}</h2>
-            <h5>{datosClase.instructor_id}</h5>
-        </div>
+        <>
+            <div className="d-flex align-items-center" >
+                <div className="m-5" >
+                    <Link to="/">
+                        <button type="button" className="btn-negro btn-circle btn-xl"> {"<<"} </button>
+                    </Link>
+                    
+                </div>
+
+                <div className="tituloClase ">
+                    <h2>{datosClase.name}</h2>  
+                    <h5>{datosClase.instructor_id}</h5>
+                </div>
+            </div>
+          
+            <div className="reproductor">
+                5
+            </div>
+          
+        </>
+        
+           
+      
     )
 }
 export default Reproductor;
